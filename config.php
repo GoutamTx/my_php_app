de9b5c0219455593b2846e6ba8cc42aa6522242f
Ayush Kumar <168015232+Ayushkr093@users.noreply.github.com>
2025-05-28 15:53:35 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER', 'mysql');
 define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'admin');
-define('DB_NAME', 'demo_db');
+define('DB_NAME', 'demo');
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if ($link === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
